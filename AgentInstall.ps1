bf8b49e09617974b15f62ba34f3fee0a7904544a
mgreenegit <migreene@outlook.com>
2016-03-21 09:51:04 -0500

    initialize repo

---
+++ AgentInstall.ps1
@@ -0,0 +1,25 @@
+﻿Configuration AgentInstall {
+# Version 1.0
+param(
+[parameter(Mandatory = $true)]
+[string]$workspaceID,
+[parameter(Mandatory = $true)]
+[string]$workspaceKey
+)
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration' - Version '3.8.0.0'
+    Import-DscResource -ModuleName 'cMMAgent' -ModuleVersion '1.2'
+    node localhost {
+        xRemoteFile OMSAgent {
+            DestinationPath =    'c:\temp\OMS\MMASetup-AMD64.exe'
+            Uri =                'http://go.microsoft.com/fwlink/?LinkID=517476'
+            MatchSource =        $False
+        }
+        cMMAgentInstall OMSAgent {
+            Path =               'c:\temp\OMS\MMASetup-AMD64.exe'
+            Ensure =             'Present'
+            WorkspaceID =        $workspaceID
+            WorkspaceKey =       $workspaceKey
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# dsc-oms
-DSC Configuration that on-boards a new VM to OMS log analytics and automation
+# 101-dsc-windowsoms
+DSC Configuration that on-boards a new VM to OMS Log Analytics
+
+This configuration requires 2 module:
+  - [cMMAgent](http://www.powershellgallery.com/packages/cMMAgent/1.2) Version 1.3
+  - [xPSDesiredStateConfiguration](http://www.powershellgallery.com/packages/xPSDesiredStateConfiguration/3.8.0.0) Version 3.8.0.0
+  
+This configuration accepts 2 parameters:
+  - **Workspace ID** is the unique identifier for an OMS Log Analytics subscription
+  - **Workspace Key** is a unique string that allows agents to write logs to OMS log analytics.  This should be considered a secret. 
+  
+Releases:
+  - **1.0** Minimum viable product.  No known issues.   
